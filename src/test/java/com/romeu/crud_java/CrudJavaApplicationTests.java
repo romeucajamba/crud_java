e52b03fb5bb77e42562e05bbaf513075ba0aab37
romeucajamba <romeucajamba@gmail.com>
2024-08-22 08:25:24 +0100

    sending struture in repository

---
+++ src/test/java/com/romeu/crud_java/CrudJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romeu.crud_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
