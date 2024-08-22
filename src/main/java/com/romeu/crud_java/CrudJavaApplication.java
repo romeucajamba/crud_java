4bdbb2af8d414afce40c77a579cfbd5f596e396b
romeucajamba <romeucajamba@gmail.com>
2024-08-22 08:54:00 +0100

    install dependency

--- src/main/java/com/romeu/crud_java/CrudJavaApplication.java
+++ src/main/java/com/romeu/crud_java/CrudJavaApplication.java
@@ -3,7 +3,7 @@ package com.romeu.crud_java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //classe de entrada da minha aplicação, que vai iniciar tudo.
 public class CrudJavaApplication {
 
 	public static void main(String[] args) {
