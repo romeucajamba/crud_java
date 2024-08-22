77ec51c467ca3e9482eb40ae633c7800b07b8ee5
romeucajamba <romeucajamba@gmail.com>
2024-08-22 16:54:14 +0100

    add endpoit delete

--- src/main/java/com/romeu/crud_java/controllers/ProductController.java
+++ src/main/java/com/romeu/crud_java/controllers/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,5 +59,16 @@ public class ProductController {
     
         return ResponseEntity.ok(product);
     }
-    
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer id) {
+        Optional<Product> product = repository.findById(id);
+        
+        if (product.isEmpty()) {
+            return ResponseEntity.notFound().build(); // Retorna 404 se o produto não for encontrado
+        }
+        
+        repository.deleteById(id);
+        return ResponseEntity.ok().build(); 
+    }    
 }
